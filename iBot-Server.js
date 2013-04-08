68c0c1d9a54e247b44896c37f8dc907649ee6892
Dash <dash@green.shockk.co.uk>
2013-04-08 23:21:18 +0000

    Wrote some more base Server class code

--- iBot-Server.js
+++ iBot-Server.js
@@ -1,7 +1,16 @@
+var User = require('iBot-User.js');
+
 exports.Server = function(host, port, nick, ident, pass)
 {
 	this.host = host;
 	this.port = port;
 	this.nick = nick;
 	this.ident = ident;
+	this.pass = pass;
+
+	this.users = {};
+	this.channels = {};
+
+	this.user = new User(nick, ident, '', 'iBot');
+	this.users[nick] = this.user;
 }
