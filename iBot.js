710d23da6c74625b574a825abfbe22e3ddfe25f0
Dash <dash@green.shockk.co.uk>
2013-04-09 22:47:59 +0000

    Wrote iBot.js for all class exports

---
+++ iBot.js
@@ -0,0 +1,5 @@
+exports.Context = require('./iBot-Context.js');
+exports.Server = require('./iBot-Server.js');
+exports.User = require('./iBot-User.js');
+exports.Channel = require('./iBot-Channel.js');
+exports.Mode = require('./iBot-Mode.js');
