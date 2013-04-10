38c7d1ac5e4ae788b5d0d320aa05bde57f27bb26
Shockk <shockk@green.shockk.co.uk>
2013-04-10 23:09:20 +0000

    Fixed syntax error in index.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var iBot = require('./iBot.js');
 var ctx = new iBot.Context();
 
-ctx.servers['dv'] = new iBot.Server('irc.dreamviews.com', 6667, 'iBot^^', 'iBot' true);
+ctx.servers['dv'] = new iBot.Server('irc.dreamviews.com', 6667, 'iBot^^', 'iBot', true);
 ctx.start();
 
 //iBot.exit();
