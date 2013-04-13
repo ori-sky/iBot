253f6fa6b6194d70cefa49143145ba0b76e54b90
Shockk <shockk@green.shockk.co.uk>
2013-04-13 02:05:27 +0000

    Fixed reference to addModule in index.

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var iBot = require('./iBot.js');
 var ctx = new iBot.Context();
 
 ctx.servers['dv'] = new iBot.Server('irc.dreamviews.com', 6667, 'iBot^^', 'iBot', true);
-ctx.servers['dv'].addModule('core');
+ctx.servers['dv'].loadModule('core');
 
 ctx.start();
