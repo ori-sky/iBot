98f8ee21d44e59f45274f9bfd2db03b32c9eecc9
Shockk <shockk@green.shockk.co.uk>
2013-04-13 04:03:41 +0000

    Re-added Dreamviews test with PonyChat to test multiple servers.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var iBot = require('./iBot.js');
 var ctx = new iBot.Context();
 
-//ctx.servers['dv'] = new iBot.Server('irc.dreamviews.com', 6667, 'iBot^^', 'iBot', true);
-//ctx.servers['dv'].loadModule('core');
+ctx.servers['dv'] = new iBot.Server('irc.dreamviews.com', 6667, 'iBot^^', 'iBot', true);
+ctx.servers['dv'].loadModule('core');
 
 ctx.servers['pony'] = new iBot.Server('irc.ponychat.net', 6667, 'iBot', 'iBot', false);
 ctx.servers['pony'].loadModule('core');
