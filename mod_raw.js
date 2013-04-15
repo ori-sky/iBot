358723cabfa39b4227f381444fcbbed7d805118e
Shockk <shockk@green.shockk.co.uk>
2013-04-15 13:01:08 +0000

    Made general fixes to mod_raw.

--- mod_raw.js
+++ mod_raw.js
@@ -4,16 +4,13 @@ exports.mod = function(context)
 	{
 		switch(opcode)
 		{
-			case 'PING':
-				server.send('PONG :' + params[0]);
-				break;
 			case 'PRIVMSG':
 				var words = params[1].split(' ');
 
 				switch(words[0])
 				{
-					case '!_raw':
-						if(server.master.test(prefix['mask']))
+					case '!raw':
+						if(server.master.test(prefix.mask))
 						{
 							server.send(words.slice(1).join(' '));
 						}
