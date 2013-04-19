d0c7e24bb00d9f366d8aa860e8ea6ef1503565dc
Shockk <shockk@green.shockk.co.uk>
2013-04-19 10:29:13 +0000

    Added a LOT of broken stuff. Transitioning to better repo structure.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-var iBot = require('./iBot.js');
-var ctx = new iBot.Context();
+var path = require('path');
 
-ctx.servers['dv'] = new iBot.Server(ctx, 'irc.dreamviews.com', 6667, 'iBot^^', 'iBot', true);
+var Context = require('iBot-Context').Context;
+var Server = require('iBot-Server').Server;
+
+var ctx = new Context(
+{
+	modulesPath: path.resolve('./modules/')
+});
+
+ctx.servers['dv'] = new Server(ctx, 'irc.dreamviews.com', 6667, 'iBot^^', 'iBot', true);
 ctx.servers['dv'].master = /.+!.+@staff\.dreamviews\.com/;
 
-ctx.servers['pony'] = new iBot.Server(ctx, 'irc.ponychat.net', 6667, 'iBot', 'iBot', false);
+ctx.servers['pony'] = new Server(ctx, 'irc.ponychat.net', 6667, 'iBot', 'iBot', false);
 ctx.servers['pony'].master = /.+!.+@shockk\.pony/;
 
 ctx.loadModule('core', null);
