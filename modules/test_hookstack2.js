0897fee344a6713483d134fa32a6042f62b9879d
Shockk <shockk@green.shockk.co.uk>
2013-04-23 10:27:45 +0000

    Removed merged test modules from experimental branch.

--- modules/test_hookstack2.js
+++
@@ -1,9 +0,0 @@
-exports.mod = function(server)
-{
-	this.test_hookstack$depth2 = function(server, target)
-	{
-		server.send('PRIVMSG ' + target + ' :depth2 before');
-		server.fire('depth3', server, target);
-		server.send('PRIVMSG ' + target + ' :depth2 after');
-	}
-}
