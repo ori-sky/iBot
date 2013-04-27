3d52c7652c986702ce0ab9ed949ce51b7c6889bb
Shockk <shockk@green.shockk.co.uk>
2013-04-27 15:13:17 +0000

    Wrote mod log to begin removing log functionality from iBot itself.

---
+++ modules/log.js
@@ -0,0 +1,35 @@
+var util = require('util');
+
+exports.mod = function(context)
+{
+	this.channels =
+	{
+		urgent: process.stdout,
+		out: process.stdout,
+		err: process.stderr,
+		verbose: null,
+	};
+
+	this.$log = function(data, channel)
+	{
+		this.log(data, channel)
+	}
+
+	this.log = function(data, channel)
+	{
+		this.logUnsafe(data, channel);
+	}
+
+	this.logUnsafe = function(data, channel)
+	{
+		var d = new Date();
+		var t = '[' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + ']';
+
+		if(typeof this.channels[channel] === 'undefined') channel = null;
+
+		if(this.channels[channel] !== null)
+		{
+			this.channels[channel].write(t + ' ' + data + '\n');
+		}
+	}
+}
