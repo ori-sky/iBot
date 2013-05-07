cf8b13ad2da7b1a94625161d85f9c816fe9d9aad
Shockk <shockk@green.shockk.co.uk>
2013-05-07 22:25:41 +0000

    Wrote iBot.start which -should- work once other code is written.

--- index.js
+++ index.js
@@ -5,8 +5,30 @@ module.exports =
 	User:		require('./iBot-User'),
 	Channel:	require('./iBot-Channel'),
 	Mode:		require('./iBot-Mode'),
-	start: function()
+	start: function(path)
 	{
-		
+		var fs = require('fs');
+
+		var ctx = undefined;
+
+		if(path === undefined) path = process.cwd() + '/config.json';
+
+		fs.readFile(path, function(err, data)
+		{
+			if(err) console.log(err);
+			else
+			{
+				try
+				{
+					var config = JSON.parse(data);
+					ctx = new module.exports.Context(config);
+				}
+				catch(e)
+				{
+					console.log('Errors parsing config file:');
+					console.log(e.stack);
+				}
+			}
+		});
 	}
 };
