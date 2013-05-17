1a2ab0cc7a516da8ddd6c094c8a18aef16509721
Shockk <shockk@green.shockk.co.uk>
2013-05-17 23:29:25 +0000

    Added scheduler module (unstable).

---
+++ modules/scheduler.js
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2013, David Farrell <shokku.ra@gmail.com>
+ *  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * - Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * 
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * 
+ * - Neither the name of iBot nor the names of its contributors may be used to
+ *   endorse or promote products derived from this software without specific
+ *   prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+exports.mod = function(context)
+{
+	this._load = function(data)
+	{
+
+	}
+
+	this._save = function()
+	{
+
+	}
+
+	this._suspend = function()
+	{
+
+	}
+
+	this._resume = function(data)
+	{
+
+	}
+
+	this.core$cmd = function(server, prefix, target, cmd, params)
+	{
+		if(cmd === 'scheduler')
+		{
+			switch(params[0])
+			{
+				case '+':
+					server.do('scheduler$fire', server, [5000], undefined, 'test', server, target);
+					break;
+			}
+		}
+	}
+
+	this._fire = function()
+	{
+		var args = Array.prototype.slice.call(arguments, 0);
+		var server = args[0];
+		var timespan = args[1];
+		var name = args[2];
+
+		if(server === undefined) throw new Error('Server undefined');
+
+		var ms = 0;
+
+		if(typeof timespan === 'object')
+		{
+			if(typeof timespan[0] === 'number') ms = timespan[0];
+		}
+		else if(typeof timespan === 'number')
+		{
+			ms = timespan;
+		}
+
+		var a = [ms, name].concat(args.slice(3));
+
+		//concat(args.slice(3));
+		server.fireTimed.apply(server, a);
+	}
+
+	this.scheduler$test = function(server, target)
+	{
+		server.send('PRIVMSG ' + target + ' :fired');
+	}
+}
