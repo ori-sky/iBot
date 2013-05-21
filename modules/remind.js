c0169e17844cb2bea466e4bdcb81acc630439de6
Shockk <shockk@green.shockk.co.uk>
2013-05-21 19:47:14 +0000

    Updated mod remind to use scheduler$timeToMilliseconds function and removed its own similar function.

--- modules/remind.js
+++ modules/remind.js
@@ -36,10 +36,10 @@ exports.mod = function(context)
 		switch(cmd)
 		{
 			case 'remind':
-				server.do('scheduler$fire', server, this.timeToMilliseconds(params[0]), params[1], 'remind$remind', server, prefix.nick, target, params.slice(2).join(' '));
+				server.do('scheduler$fire', server, params[0], params[1], 'remind$remind', server, prefix.nick, target, params.slice(2).join(' '));
 				break;
 			case 'remindme':
-				server.do('scheduler$fire', server, this.timeToMilliseconds(params[0]), params[1], 'remind$remind', server, prefix.nick, prefix.nick, params.slice(2).join(' '));
+				server.do('scheduler$fire', server, params[0], params[1], 'remind$remind', server, prefix.nick, prefix.nick, params.slice(2).join(' '));
 				break;
 		}
 	}
@@ -48,34 +48,5 @@ exports.mod = function(context)
 	{
 		console.log(4567);
 		server.do('core$privmsg', server, to, 'Reminder from ' + from + ': ' + msg);
-	}
-
-	this.timeToMilliseconds = function(timeString)
-	{
-		//var r = /(\d+y)?(\d+w)?(\d+d)?(\d+h)?(\d+m)?(\d+s)?(\d+)?/;
-		var r = /(\d+d)?(\d+h)?(\d+m)?(\d+s)?(\d+)?/;
-		var match = r.exec(timeString);
-
-		var t = 0;
-
-		// this won't take leap years into account
-		// years and weeks don't work presumably because max integer size is too big
-		/*
-		if(typeof match[1] !== 'undefined') t += parseInt(match[1]) * 1000 * 60 * 60 * 24 * 7 * 365;
-		if(typeof match[2] !== 'undefined') t += parseInt(match[2]) * 1000 * 60 * 60 * 24 * 7;
-		if(typeof match[3] !== 'undefined') t += parseInt(match[3]) * 1000 * 60 * 60 * 24;
-		if(typeof match[4] !== 'undefined') t += parseInt(match[4]) * 1000 * 60 * 60;
-		if(typeof match[5] !== 'undefined') t += parseInt(match[5]) * 1000 * 60;
-		if(typeof match[6] !== 'undefined') t += parseInt(match[6]) * 1000;
-		if(typeof match[7] !== 'undefined') t += parseInt(match[7]);
-		*/
-
-		if(typeof match[1] !== 'undefined') t += parseInt(match[1]) * 1000 * 60 * 60 * 24;
-		if(typeof match[2] !== 'undefined') t += parseInt(match[2]) * 1000 * 60 * 60;
-		if(typeof match[3] !== 'undefined') t += parseInt(match[3]) * 1000 * 60;
-		if(typeof match[4] !== 'undefined') t += parseInt(match[4]) * 1000;
-		if(typeof match[5] !== 'undefined') t += parseInt(match[5]);
-
-		return t;
 	}
 }
