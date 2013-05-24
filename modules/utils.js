a0bde131fbcadde6b43ae953974c0f26605b9c7f
Shockk <shockk@green.shockk.co.uk>
2013-05-24 02:03:45 +0000

    Refactored mod utils and added memusage command.

--- modules/utils.js
+++ modules/utils.js
@@ -1,20 +1,28 @@
 var crypto = require('crypto');
+var util = require('util');
 
 exports.mod = function(context, server)
 {
-	this.core$cmd = function(prefix, target, cmd, params)
+	this.core$cmd = function(prefix, target, cmd, params, $core)
 	{
 		switch(cmd)
 		{
 			case 'ping':
-				server.send('PRIVMSG ' + target + ' :pong' + (params[0] !== undefined ? (' ' + params[0]) : ''));
+				$core._privmsg(target, 'pong' + (params[0] !== undefined ? (' ' + params[0]) : ''));
 				break;
 			case 'digest':
-				if(params.length < 2) { server.send('PRIVMSG ' + target + ' :Syntax: hash <type> <string>'); break; }
+				if(params.length < 2)
+				{
+					$core._privmsg(target, 'Syntax: hash <type> <string>');
+					break;
+				}
 
 				var h = crypto.createHash(params[0]);
 				h.update(params[1]);
-				server.send('PRIVMSG ' + target + ' :Hash: ' + h.digest('hex'));
+				$core._privmsg(target, 'Hash: ' + h.digest('hex'));
+				break;
+			case 'memusage':
+				$core._privmsg(target, util.inspect(process.memoryUsage()));
 				break;
 		}
 	}
