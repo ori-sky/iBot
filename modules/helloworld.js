594003d36fb16fca6c4a11199c15947e10d84dee
Shockk <shokku.ra@gmail.com>
2013-06-28 15:52:21 +0000

    Implemented help in mod helloworld.

--- modules/helloworld.js
+++ modules/helloworld.js
@@ -1,5 +1,20 @@
 exports.mod = function(context, server)
 {
+	this.help$register = function()
+	{
+		server.do('help$register', 'helloworld');
+	}
+
+	this._help = function(topic, params)
+	{
+		switch(topic)
+		{
+			case 'helloworld':
+				return 'Prints a hello message.';
+				break;
+		}
+	}
+
 	this.core$cmd = function(prefix, target, command, params, $core)
 	{
 		if(command === 'helloworld') $core._privmsg(target, 'Hello, ' + prefix.nick + '!');
